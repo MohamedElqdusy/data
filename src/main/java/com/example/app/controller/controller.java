1152bb43f64d49ffb3828ebd031e35bbb21f6d6a
Mohamed Elqdusy <mohamedelqdusy@gmail.com>
2018-06-21 15:55:11 +0200

    Adding real time messaging feature
    created a webpage to display message delivery in real time to the browser client

--- src/main/java/com/example/app/controller/controller.java
+++ src/main/java/com/example/app/controller/controller.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.HtmlUtils;
 
 import com.example.app.model.Message;
-import com.example.app.repository.MessageRepository;
 import com.example.app.service.MessageService;
 
 @RestController
@@ -19,9 +20,13 @@ public class controller {
 	@Autowired
 	MessageService messageService;
 
+	@Autowired
+	private SimpMessagingTemplate template;
+
 	@RequestMapping(value = "/add", method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
 	public void create(@RequestBody Message message) {
 		messageService.save(message);
+		template.convertAndSend("/topic/message", HtmlUtils.htmlEscape(message.getMessage().toString()));
 	}
 
 	@RequestMapping("/messages")
---
+++ src/main/resources/static/app.js
@@ -0,0 +1,43 @@
+var stompClient = null;
+
+function setConnected(connected) {
+    $("#connect").prop("disabled", connected);
+    $("#disconnect").prop("disabled", !connected);
+    if (connected) {
+        $("#conversation").show();
+    }
+    else {
+        $("#conversation").hide();
+    }
+    $("#message").html("");
+}
+
+function connect() {
+    var socket = new SockJS('/webserver');
+    stompClient = Stomp.over(socket);
+    stompClient.connect({}, function (frame) {
+        setConnected(true);
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/topic/message', function (message) {
+            showMessage(message.body);
+        });
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    setConnected(false);
+    console.log("Disconnected");
+}
+
+function showMessage(message) {
+    $("#message").append("<tr><td>" + message + "</td></tr>");
+}
+
+$(function () {
+    $( "#connect" ).click(function() { connect(); });
+    $( "#disconnect" ).click(function() { disconnect(); });
+});
+
